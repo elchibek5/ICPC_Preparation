35be7bc5f105ec5d78eaa469a01797c6c4be17c7
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-08-12 04:13:52 -0500

    Update SortColors.java

--- medium/SortColors.java
+++ medium/SortColors.java
@@ -1,13 +1,15 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int position = 0;
+        int low = 0, cur = 0, high = nums.length - 1;
         
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) swap(nums, i, position++);
-        }
-
-        for (int i = position; i < nums.length; i++) {
-            if (nums[i] == 1) swap(nums, i, position++);
+        while (cur <= high) {
+            if (nums[cur] == 0) {
+                swap(nums, low++, cur++);
+            } else if (nums[cur] == 1) {
+                cur++;
+            } else {
+                swap(nums, cur, high--);
+            }
         }
     }
 
@@ -17,6 +19,26 @@ class Solution {
         nums[j] = temp;
     }
 }
+
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int position = 0;
+        
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] == 0) swap(nums, i, position++);
+//         }
+
+//         for (int i = position; i < nums.length; i++) {
+//             if (nums[i] == 1) swap(nums, i, position++);
+//         }
+//     }
+
+//     private void swap(int[] nums, int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+// }
 
 // class Solution {
 //     public void sortColors(int[] nums) {
