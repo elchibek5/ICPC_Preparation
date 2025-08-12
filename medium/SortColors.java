aed86e17aacd51d3ac44d1692942c5b52ab3536b
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-08-12 03:48:38 -0500

    Update SortColors.java

--- medium/SortColors.java
+++ medium/SortColors.java
@@ -1,18 +1,38 @@
 class Solution {
     public void sortColors(int[] nums) {
-        boolean swapped = true;
+        int position = 0;
         
-        while (swapped) {
-            swapped = false;
-            for (int i = 1; i < nums.length; i++) {
-                if (nums[i] < nums[i - 1]) {
-                    int answer = 0;
-                    answer = nums[i];
-                    nums[i] = nums[i - 1];
-                    nums[i - 1] = answer;
-                    swapped = true;
-                } 
-            }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) swap(nums, i, position++);
+        }
+
+        for (int i = position; i < nums.length; i++) {
+            if (nums[i] == 1) swap(nums, i, position++);
         }
     }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
+
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         boolean swapped = true;
+        
+//         while (swapped) {
+//             swapped = false;
+//             for (int i = 1; i < nums.length; i++) {
+//                 if (nums[i] < nums[i - 1]) {
+//                     int answer = 0;
+//                     answer = nums[i];
+//                     nums[i] = nums[i - 1];
+//                     nums[i - 1] = answer;
+//                     swapped = true;
+//                 } 
+//             }
+//         }
+//     }
+// }
