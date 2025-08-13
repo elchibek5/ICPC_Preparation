41a2b2e0769fbf5e6b1085b9305fa718a94909c0
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-08-13 17:26:14 -0500

    Update ValidParentheses.java

--- easy/ValidParentheses.java
+++ easy/ValidParentheses.java
@@ -1,26 +1,40 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stringStack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stringStack.push(ch);
-            }
-            
-            else if (ch == '}' || ch == ')' || ch == ']') {
-                if (stringStack.isEmpty()) {
-                    return false;
-                }
-                char top = stringStack.pop();
-                if ((ch == ')' && top != '(') ||
-                    (ch == '}' && top != '{') ||
-                    (ch == ']' && top != '[')) {
-                        return false;
-                    }
-            }
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '[') stack.push(']');
+            else if (c == '{') stack.push('}');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
         }
-        return stringStack.isEmpty();
+        return stack.isEmpty();
     }
 }
+
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stringStack = new Stack<>();
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+
+//             if (ch == '(' || ch == '{' || ch == '[') {
+//                 stringStack.push(ch);
+//             }
+            
+//             else if (ch == '}' || ch == ')' || ch == ']') {
+//                 if (stringStack.isEmpty()) {
+//                     return false;
+//                 }
+//                 char top = stringStack.pop();
+//                 if ((ch == ')' && top != '(') ||
+//                     (ch == '}' && top != '{') ||
+//                     (ch == ']' && top != '[')) {
+//                         return false;
+//                     }
+//             }
+//         }
+//         return stringStack.isEmpty();
+//     }
+// }
