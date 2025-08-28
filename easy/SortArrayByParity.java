e63cf51ef58221a77a5e7b015507a222a3647486
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-08-28 18:48:56 -0500

    Update SortArrayByParity.java

--- easy/SortArrayByParity.java
+++ easy/SortArrayByParity.java
@@ -2,16 +2,14 @@ class Solution {
     public int[] sortArrayByParity(int[] nums) {
         int pointer = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
+            if ((nums[i] & 1) == 0) {
                 swap(nums, pointer++, i);
             }
         }
         return nums;
     }
 
-    private void swap(int[] nums, int odd, int even) {
-        int temp = nums[even];
-        nums[even] = nums[odd];
-        nums[odd] = temp;
+    private void swap(int[] a, int x, int y) {
+        int t = a[x]; a[x] = a[y]; a[y] = t;
     }
 }
