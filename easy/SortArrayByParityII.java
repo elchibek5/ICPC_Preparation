678712ce77531eb95e8d56c9e9bea6b4bad8c2af
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-08-28 18:23:36 -0500

    Update SortArrayByParityII.java

--- easy/SortArrayByParityII.java
+++ easy/SortArrayByParityII.java
@@ -5,20 +5,24 @@ class Solution {
         int even = 1;
         
         while(odd < sum && even < sum) {
-            if (nums[odd] % 2 == 0) {
+            while (odd < sum && nums[odd] % 2 == 0) {
                 odd += 2;
-            } else if (nums[even] % 2 == 1) {
+            } 
+            
+            while (even < sum && nums[even] % 2 == 1) {
                 even += 2;
-            } else {
-                int temp = nums[odd];
-                temp = nums[odd];
-                nums[odd] = nums[even];
-                nums[even] = temp;
-
-                odd += 2;
-                even += 2;
+            } 
+            
+            if (odd < sum && even < sum) {
+                swap(nums, odd, even);
             }
         }
         return nums;
+    }
+
+    private void swap(int[] nums, int odd, int even) {
+        int temp = nums[odd];
+        nums[odd] = nums[even];
+        nums[even] = temp;
     }
 }
