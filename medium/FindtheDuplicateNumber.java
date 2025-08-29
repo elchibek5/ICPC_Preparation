0e7b3ef834f4561c80fab81fce338f1735880ea6
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-08-29 18:37:41 -0500

    Update FindtheDuplicateNumber.java

--- medium/FindtheDuplicateNumber.java
+++ medium/FindtheDuplicateNumber.java
@@ -1,9 +1,19 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        HashSet<Integer> seen = new HashSet<>();
-        for (int x : nums) {
-            if (!seen.add(x)) return x;  
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1; 
+        return slow;
+
     }
 }
