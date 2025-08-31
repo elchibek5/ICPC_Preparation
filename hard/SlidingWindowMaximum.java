3d04cfeca464c655e06202681c6b97198ab45fb5
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-08-31 17:55:44 -0500

    Update SlidingWindowMaximum.java

--- hard/SlidingWindowMaximum.java
+++ hard/SlidingWindowMaximum.java
@@ -1,23 +1,33 @@
-// Logic works, but run time error
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        if (n == 1) return nums;
-
-        int[] answer = new int[n - k + 1];
-        int left = 0;
-        int right = k;
-        int idx = 0;
-
-        while (right <= n) {
-            int max = Integer.MIN_VALUE;
-            for (int i = left; i < right; i++) {
-                if (nums[i] > max) max = nums[i];
-            }
-            answer[idx++] = max;
-            left++;
-            right++;
+        int sqrt = (int) Math.ceil(Math.sqrt(n));
+        int[] block_max = new int[sqrt];
+        Arrays.fill(block_max, Integer.MIN_VALUE);
+        for (int i = 0; i < n; i++) {
+            int block = i / sqrt;
+            block_max[block] = Math.max(block_max[block], nums[i]);
         }
-        return answer;
+
+        int[] results = new int[n - k + 1];
+
+        for (int i = 0; i + k - 1 < n; i++) {
+            int l = i;
+            int r = i + k - 1;
+
+            int result = Integer.MIN_VALUE;
+            for (int j = l; j <= r; ) {
+                if (j % sqrt == 0 && j + sqrt - 1 <= r) {
+                    int block = j / sqrt;
+                    result = Math.max(result, block_max[block]);
+                    j += sqrt;
+                } else {
+                    result = Math.max(result, nums[j]);
+                    j++;
+                }
+            }
+            results[i] = result;
+        }
+        return results;
     }
 }
