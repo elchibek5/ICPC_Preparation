3e43c174a02c26fa58f071b9f0b2630136dcd9f1
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-09-14 01:13:53 -0500

    Update MinimumOperationstoExceedThresholdValueI.java

--- easy/MinimumOperationstoExceedThresholdValueI.java
+++ easy/MinimumOperationstoExceedThresholdValueI.java
@@ -1,13 +1,9 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        if (k == 1) return 0;
-
-        Arrays.sort(nums);
         int answer = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= k) return answer;
-            answer++;
+            if (nums[i] < k) answer++;
         }
         return answer;
     }
