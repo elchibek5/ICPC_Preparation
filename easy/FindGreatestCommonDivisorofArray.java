2f88e3304e1c606bd83928dbcd95853572e992db
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-09-18 17:41:48 -0500

    Update FindGreatestCommonDivisorofArray.java

--- easy/FindGreatestCommonDivisorofArray.java
+++ easy/FindGreatestCommonDivisorofArray.java
@@ -1,9 +1,13 @@
 class Solution {
     public int findGCD(int[] nums) {
-        Arrays.sort(nums);
-        int min = nums[0];
-        int max = nums[nums.length - 1];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
+        for (int n : nums) {
+            if (n < min) min = n;
+            if (n > max) max = n;
+        }
+        
         while (min != 0) {
             int t = max % min;
             max = min;
