0d433869a07dcf5df843100b31e72f46db8557a0
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-09-18 19:14:14 -0500

    Update FindMinimuminRotatedSortedArray.java

--- medium/FindMinimuminRotatedSortedArray.java
+++ medium/FindMinimuminRotatedSortedArray.java
@@ -1,10 +1,27 @@
 class Solution {
     public int findMin(int[] nums) {
-        int min = Integer.MAX_VALUE;
+        int n = nums.length;
 
-        for (int n : nums) {
-            if (n < min) min = n;
+        if (nums == null || n == 0) return 0;
+
+        if (n == 1) return nums[0];
+
+        if (nums[0] < nums[n - 1]) return nums[0];
+
+        int left = 0;
+        int right = n - 1;
+        //[4,5,6,7,0,1,2]
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (middle > 0 && nums[middle - 1] > nums[middle]) return nums[middle];
+
+            if (nums[middle] > nums[right]) left = middle + 1;
+            else right = middle - 1;
         }
-        return min;
+
+        return -1;
+
     }
 }
