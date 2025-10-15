b8f1c54ad628993d2cf5adb2f9690547b7e61381
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-10-15 16:58:38 -0500

    Implement memoization in Fibonacci function
    Refactor Fibonacci calculation to use memoization.

--- easy/FibonacciNumber.java
+++ easy/FibonacciNumber.java
@@ -1,6 +1,16 @@
 class Solution {
-    public int fib(int n) {
+    private int f[];
+
+    private int fib_(int n) {
         if (n <= 1) return n;
-        return fib(n - 1) + fib(n - 2);
+        if (f[n] == 0) {
+            f[n] = fib_(n - 1) + fib_(n - 2);
+        }
+        return f[n];
+    }
+
+    public int fib(int n) {
+        f = new int[n + 1];
+        return fib_(n);
     }
 }
