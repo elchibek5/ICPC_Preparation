75f5ebeeedcf7dce822c950faacda7bb357c0154
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-12-02 18:14:02 -0600

    Implement recursive solution for merging lists
    Added a new solution for merging two sorted linked lists using recursion.

--- easy/MergeTwoSortedLists.java
+++ easy/MergeTwoSortedLists.java
@@ -31,3 +31,36 @@ class Solution {
         return dummy.next;
     }
 }
+// SOLUTION 2
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return mergeLinkedList(list1, list2);
+    }
+
+    private ListNode mergeLinkedList(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val <= l2.val) {
+            l1.next = mergeLinkedList(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeLinkedList(l1, l2.next);
+            return l2;
+        }
+    }
+}
