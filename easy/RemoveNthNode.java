50c0576c6bd801da2647b54f672cd483a36b6cd7
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-12-04 20:18:24 -0600

    Implement recursive solution for removing Nth node
    Added a recursive solution to remove the Nth node from the end of a linked list.

--- easy/RemoveNthNode.java
+++ easy/RemoveNthNode.java
@@ -26,3 +26,41 @@ class Solution {
         return dummy.next;
     }
 }
+
+// SOLUTION 2 with recurtion
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ListNode start_point;
+
+    int rec(ListNode cur, ListNode parent, int n) {
+        if (cur == null) {
+            return 0;
+        } else {
+            int index_from_end = rec(cur.next, cur, n) + 1;
+            if (index_from_end == n) {
+                if (cur == start_point) {
+                    start_point = cur.next;
+                } else {
+                    parent.next = cur.next;
+                }
+            }
+            return index_from_end;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        start_point = head;
+        rec(head, null, n);
+        return start_point;
+    }
+}
