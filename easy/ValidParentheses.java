fead1f9794c396308bb4444d180d3795958a95dc
Elchibek Dastanov <elchibekdastanov@gmail.com>
2025-12-12 00:11:13 -0600

    Refactor isValid method for parentheses validation

--- easy/ValidParentheses.java
+++ easy/ValidParentheses.java
@@ -1,16 +1,42 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
 
-        for (char c : s.toCharArray()) {
-            if (c == '(') stack.push(')');
-            else if (c == '[') stack.push(']');
-            else if (c == '{') stack.push('}');
-            else if (stack.isEmpty() || stack.pop() != c) return false;
+            if (c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.peek();
+                if ((c == ']' && top == '[') || (c == ')' && top == '(') || (c == '}' && top == '{')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
         }
+
         return stack.isEmpty();
     }
 }
+
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stack = new Stack<>();
+
+//         for (char c : s.toCharArray()) {
+//             if (c == '(') stack.push(')');
+//             else if (c == '[') stack.push(']');
+//             else if (c == '{') stack.push('}');
+//             else if (stack.isEmpty() || stack.pop() != c) return false;
+//         }
+//         return stack.isEmpty();
+//     }
+// }
 
 // class Solution {
 //     public boolean isValid(String s) {
