fd57abc7ea562fc423203541629e0f7973b8e4f0
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-01-16 00:15:39 -0600

    Refactor dailyTemperatures to use Stack for NGE

--- medium/DailyTemperatures.java
+++ medium/DailyTemperatures.java
@@ -1,19 +1,40 @@
+// class Solution {
+//     public int[] dailyTemperatures(int[] temp) {
+//         int n = temp.length;
+//         int[] ans = new int[n];
+
+//         Deque<Integer> stack = new ArrayDeque<>();
+
+//         for (int i = 0; i < n; i++) {
+//             while (!stack.isEmpty() && temp[i] > temp[stack.peek()]) {
+//                 int prev = stack.pop();
+//                 ans[prev] = i - prev;
+//             }
+
+//             stack.push(i);
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int[] dailyTemperatures(int[] temp) {
         int n = temp.length;
-        int[] ans = new int[n];
+        int[] nge = new int[n];
 
-        Deque<Integer> stack = new ArrayDeque<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && temp[i] > temp[stack.peek()]) {
-                int prev = stack.pop();
-                ans[prev] = i - prev;
+        Stack<Integer> st = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && temp[st.peek()] <= temp[i]) {
+                st.pop();
             }
 
-            stack.push(i);
-        }
+            if (!st.isEmpty()) {
+                nge[i] = st.peek() - i;
+            }
 
-        return ans;
+            st.push(i);
+        }
+        return nge;
     }
 }
