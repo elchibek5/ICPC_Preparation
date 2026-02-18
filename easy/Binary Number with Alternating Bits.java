26f3ca1987cd8c8da1c3dcd222c73f0c38e21068
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-02-18 09:08:15 -0600

    Optimize hasAlternatingBits method
    Refactored hasAlternatingBits method to use bitwise operations instead of string conversion.

--- easy/Binary Number with Alternating Bits.java
+++ easy/Binary Number with Alternating Bits.java
@@ -1,13 +1,6 @@
 class Solution {
     public boolean hasAlternatingBits(int n) {
-        String binary = Integer.toBinaryString(n);
-
-        // '101'
-        for (int i = 1; i < binary.length(); i++) {
-            if (binary.charAt(i) == binary.charAt(i - 1)) {
-                return false;
-            }
-        }
-        return true;
+        int x = n ^ (n >> 1);
+        return (x & (x + 1)) == 0;
     }
 }
