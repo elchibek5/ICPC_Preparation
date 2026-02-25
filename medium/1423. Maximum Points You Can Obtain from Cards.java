b205c71e49cdd89f3a1b1cfed9d9e93e59e45751
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-02-25 17:29:52 -0600

    Implement maxScore method for card points

---
+++ medium/1423. Maximum Points You Can Obtain from Cards.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        int currentScore = 0;
+
+        for (int i = 0; i < k; i++) {
+            currentScore += cardPoints[i];
+        }
+
+        int maxScore = currentScore;
+
+        for (int i = 0; i < k; i++) {
+            currentScore -= cardPoints[k - 1 - i];
+            currentScore += cardPoints[n - 1 - i];
+
+            maxScore = Math.max(maxScore, currentScore);
+        }
+
+        return maxScore;
+    }
+}
