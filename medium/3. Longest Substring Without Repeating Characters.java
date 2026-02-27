dc448b3a6979f92794e329e3032782f32ab975d2
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-02-27 08:01:47 -0600

    Optimize substring length calculation using HashSet
    Refactor lengthOfLongestSubstring method to use HashSet for tracking characters instead of HashMap.

--- medium/3. Longest Substring Without Repeating Characters.java
+++ medium/3. Longest Substring Without Repeating Characters.java
@@ -2,19 +2,16 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         int max = 0;
         int left = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
+        HashSet<Character> set = new HashSet<>();
         
         for (int right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
-
-            map.put(c, map.getOrDefault(c, 0) + 1);
             
-            while (map.get(c) > 1) {
-                char leftChar = s.charAt(left);
-                map.put(leftChar, map.get(leftChar) - 1);
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
                 left++;
             }
-
+            set.add(c);
             max = Math.max(max, right - left + 1);
         }
         return max;
