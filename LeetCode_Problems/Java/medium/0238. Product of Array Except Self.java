dd4b782dfdfafda63e9dc7101ce0610031d6b60f
Elchibek Dastanov <elchibekdastanov@gmail.com>
2026-08-04 17:50:02 -0500

    Add solution for Product of Array Except Self

---
+++ LeetCode_Problems/Java/medium/0238. Product of Array Except Self.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        ans[0] = 1;
+        for (int i = 0; i < n - 1; i++) {
+            ans[i + 1] = ans[i] * nums[i];
+        }
+
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return ans;
+    }
+}
